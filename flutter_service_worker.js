1db7628bec297704f76e4400250bcf77ddef11bd
kastellai <ilenia.castellana@gmail.com>
2026-05-03 19:36:18 +0200

    RLS_66_v1.0.11

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8ba3c9e151cbb81dc3a606245198cefa",
+const RESOURCES = {"flutter_bootstrap.js": "c34b3387f6f1c5dae49cc046c93f55b7",
 "version.json": "9f9c34dcc25b571ea0c75552694df2ee",
 "splash/img/branding-4x.png": "e07be21b6c3fd40714ea3bcf9076cc44",
 "splash/img/branding-dark-1x.png": "6c777d5b9bc3712f7bf4955b339246a4",
@@ -15,7 +15,7 @@ const RESOURCES = {"flutter_bootstrap.js": "8ba3c9e151cbb81dc3a606245198cefa",
 "splash/img/branding-dark-3x.png": "2ee030257e77dce49855b028630ba229",
 "index.html": "2e95c5a5958adfb7c150ba4ba92a8897",
 "/": "2e95c5a5958adfb7c150ba4ba92a8897",
-"main.dart.js": "1fcf97e4b0791c5fff7d7968b59ce17c",
+"main.dart.js": "56f1a1856f27ffaede9bee1cbb5d328c",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
